bd77c7885cdd616f6287e8e29535c142166b57de
Ardiy <you@example.com>
2023-04-26 01:24:24 -0700

    test

--- main.c
+++ main.c
@@ -14,7 +14,43 @@ while (1)
 prompt();
 input = read_input();
 args = split_line(input);
+if (strcmp(args[0], "exit") == 0)
+break;
+else if (strcmp(args[0], "env") == 0)
+{
+printf("Printing environment variables:\n");
+print_environment();
+}
+else if (strcmp(args[0], "setenv") == 0)
+{
+if (args[1] == NULL || args[2] == NULL)
+{
+fprintf(stderr, "Usage: setenv VARIABLE VALUE\n");
+}
+else
+{
+int result = set_environment_variable(args[1], args[2]);
+if (result != 0)
+{
+fprintf(stderr, "Failed to set environment variable\n");
+}}}
+else if (strcmp(args[0], "unsetenv") == 0)
+{
+if (args[1] == NULL)
+{
+fprintf(stderr, "Usage: unsetenv VARIABLE\n");
+}
+else
+{
+int result = unset_environment_variable(args[1]);
+if (result != 0)
+{
+fprintf(stderr, "Failed to unset environment variable\n");
+}}}
+else
+{
 status = execute_command(args);
+}
 free(input);
 free(args);
 if (status == -1)
