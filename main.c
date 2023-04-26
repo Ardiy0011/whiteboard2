b930e4050059b3f535e9d6020c60ae9cfed602fb
Ardiy <you@example.com>
2023-04-25 23:57:39 -0700

    test

--- main.c
+++ main.c
@@ -12,44 +12,18 @@ int main(int argc, char **argv)
 char *input;
 char **args;
 int status;
-FILE *input_stream = stdin;
-if (argc > 1)
-{
-input_stream = fopen(argv[1], "r");
-if (!input_stream)
-{
-perror("Failed to open input file");
-exit(EXIT_FAILURE);
-}
-}
+
 while (1)
-{
-if (input_stream == stdin)
 {
 prompt();
 input = read_input();
-}
-else
-{
-input = read_input_from_stream(input_stream);
-if (input == NULL)
-{
-break;
-}
-printf("%s", input);
-}
 args = split_line(input);
 status = execute_command(args);
 free(input);
-free_args(args);
+free(args);
 if (status == -1)
 {
 break;
-}
-}
-if (input_stream != stdin)
-{
-fclose(input_stream);
-}
-return (0);
+}}
+return 0;
 }
