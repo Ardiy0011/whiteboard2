e1aa25c60570fa4ac6a94e85467f830dc29827cc
Ardiy <you@example.com>
2023-04-26 04:36:04 -0700

    test

--- exit_shell.c
+++ exit_shell.c
@@ -8,16 +8,27 @@
  *
  * Return: Always 0
  */
+/**
+ * exit_shell - Exit the shell program
+ * @line: Pointer to the input line
+ * @args: Array of arguments
+ * @input_stream: Pointer to the input stream
+ *
+ * Return: Always 0
+ */
 int exit_shell(char *line, char **args, FILE *input_stream)
-
 {
+if (args != NULL)
+{
+free_args(args);
+}
 if (line != NULL)
 {
 free(line);
-free_args(args);
-if (input_stream != stdin)
-fclose(input_stream);
-exit(EXIT_SUCCESS);
 }
-return (0);
+if (input_stream != stdin)
+{
+fclose(input_stream);
+}
+exit(EXIT_SUCCESS);
 }
