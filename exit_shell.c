3d356ca1ffde387e9ed19dd55fd800bc0d246efe
Ardiy <you@example.com>
2023-04-26 13:15:04 -0700

    test

--- exit_shell.c
+++ exit_shell.c
@@ -8,27 +8,28 @@
  *
  * Return: Always 0
  */
-/**
- * exit_shell - Exit the shell program
- * @line: Pointer to the input line
- * @args: Array of arguments
- * @input_stream: Pointer to the input stream
- *
- * Return: Always 0
- */
 int exit_shell(char *line, char **args, FILE *input_stream)
 {
-if (args != NULL)
-{
-free_args(args);
+  int exit_status = EXIT_SUCCESS;
+
+  if (args != NULL && args[1] != NULL)
+  {
+    exit_status = custom_atoi(args[1]);
+  }
+
+  if (args != NULL)
+  {
+    free_args(args);
+  }
+  if (line != NULL)
+  {
+    free(line);
+  }
+  if (input_stream != stdin)
+  {
+    fclose(input_stream);
+  }
+
+  exit(exit_status);
 }
-if (line != NULL)
-{
-free(line);
-}
-if (input_stream != stdin)
-{
-fclose(input_stream);
-}
-exit(EXIT_SUCCESS);
-}
+
