b9d6b4d153035fde911e680731b423d00f18c3c4
Ardiy <you@example.com>
2023-04-26 00:07:19 -0700

    test

---
+++ env_main.c
@@ -0,0 +1,13 @@
+#include "shell.h"
+/**
+ * main - The entry point of the program.
+
+ * Return: Always 0
+ */
+
+int main(void)
+{
+printf("Printing environment variables:\n");
+print_environment();
+return (0);
+}
